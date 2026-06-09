86c6bf4ba3dacb78697f87305ab6c67f9ee93bd5
RGM Developer <dev@rgm.network>
2026-06-09 10:07:12 +0300

    Fix UI: paper wallet template, currency unit RGM, remove prerelease bar, update URLs and icons

--- src/qt/rgmstrings.cpp
+++ src/qt/rgmstrings.cpp
@@ -11,7 +11,7 @@
 static const char UNUSED *bitcoin_strings[] = {
 QT_TRANSLATE_NOOP("bitcoin-core", "RGM Core"),
 QT_TRANSLATE_NOOP("bitcoin-core", "The %s developers"),
-QT_TRANSLATE_NOOP("bitcoin-core", "Bitcoin Core and RGM Core"),
+QT_TRANSLATE_NOOP("bitcoin-core", "RGM Core"),
 QT_TRANSLATE_NOOP("bitcoin-core", ""
 "(1 = keep tx meta data e.g. account owner and payment request information, 2 "
 "= drop tx meta data)"),
